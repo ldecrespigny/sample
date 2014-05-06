ca8740adcfc8c578e7a227f259cec1250fd2b1c3
Lachlan <ldecrespigny@gmail.com>
2014-05-06 10:45:55 -0400

    added profile page strucutre

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,16 +3,21 @@ class PagesController < ApplicationController
 
   	@user = User.all
 
-require 'oauth'
-require 'json'
-require 'rest_client'
- 
-# Fill the keys and secrets you retrieved after registering your app
-api_key = ENV['LINKEDIN_API_KEY']
-api_secret = ENV['LINKEDIN_SECRET_KEY']
+		require 'oauth'
+		require 'json'
+		require 'rest_client'
+		 
+		# Fill the keys and secrets you retrieved after registering your app
+		api_key = ENV['LINKEDIN_API_KEY']
+		api_secret = ENV['LINKEDIN_SECRET_KEY']
 
 
  
 
   end
+
+  def profile
+  	 @user = User.all
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users,
   controllers: {omniauth_callbacks: "omniauth_callbacks"}
   
-  root "pages#home"
+  root "pages#profile"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
